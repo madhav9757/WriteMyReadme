61e6b77efad5fd2fa6d21add6f2554958f95bbd0
madhav9757 <madhavsemwal9@gmail.com>
2025-12-26 14:15:34 +0530

    ....

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ app.use(
 /* ---------------------------- CORS Setup ------------------------------- */
 app.use(
   cors({
-    origin: ENV.CORS_ORIGIN,
+    origin: "https://write-my-readme-qc99f.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
