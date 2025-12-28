e0bb4cd008e304c2cdd4e8d1ca5ce2e78263a3bc
madhav9757 <madhavsemwal9@gmail.com>
2025-12-28 13:15:16 +0530

    test 9

--- backend/src/app.js
+++ backend/src/app.js
@@ -39,6 +39,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Cookie"],
+    optionsSuccessStatus: 200
   })
 );
 
